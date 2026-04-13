cc77694c619cdf73265ed99d76ab09fd177a8985
Otsuka Daichi <109046865+KujataD@users.noreply.github.com>
2026-04-14 08:57:21 +0900

    MT3_01_00

--- MT3/Matrix4x4.cpp
+++ MT3/Matrix4x4.cpp
@@ -242,3 +242,43 @@ Matrix4x4 Matrix4x4::MakeAffineMatrix(const Vector3& scale, const Vector3& rotat
 	Matrix4x4 w = s * r * t;
 	return w;
 }
+
+
+Matrix4x4 Matrix4x4::MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) { 
+	return {
+		{
+			{(1.0f / std::tan(fovY / 2.0f)) / aspectRatio, 0.0f, 0.0f, 0.0f},
+			{0.0f, (1.0f / std::tan(fovY / 2.0f)), 0.0f, 0.0f},
+			{0.0f, 0.0f, farClip / (farClip - nearClip), 1.0f},
+			{0.0f, 0.0f, -nearClip * farClip / (farClip - nearClip) , 0.0f}
+		}
+	};
+}
+
+Matrix4x4 Matrix4x4::MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	return {
+	    {
+			{2.0f / (right - left), 0.0f, 0.0f, 0.0f},
+		    {0.0f, 2.0f / (top - bottom), 0.0f, 0.0f},
+		    {0.0f, 0.0f, 1.0f / (farClip - nearClip), 0.0f}, 
+			{(left + right) / (left - right), (top + bottom) / (bottom - top), nearClip / (nearClip  - farClip), 1.0f}
+		}
+	};
+}
+
+Matrix4x4 Matrix4x4::MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	if (minDepth > maxDepth) {
+		float tmp = minDepth;
+		minDepth = maxDepth;
+		maxDepth = tmp;
+	}
+
+	return {
+	    {
+			{width / 2.0f, 0.0f, 0.0f, 0.0f},
+			{0.0f, -(height / 2.0f), 0.0f, 0.0f},
+			{0.0f, 0.0f, maxDepth - minDepth, 0.0f}, 
+			{left + (width / 2.0f), top + (height / 2.0f), minDepth, 1.0f}
+		}
+	};
+}
